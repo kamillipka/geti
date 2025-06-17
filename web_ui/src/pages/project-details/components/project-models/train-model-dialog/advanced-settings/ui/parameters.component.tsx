394b079b344b0a5f38389383c77cccf8f7466d0c
Dawid Wesołowski <dawid.wesolowski@intel.com>
2025-06-17 09:33:47 +0200

    ITEP-32138 Integrate learning parameters from new training configuration [PART 3] (#459)

--- web_ui/src/pages/project-details/components/project-models/train-model-dialog/advanced-settings/ui/parameters.component.tsx
+++ web_ui/src/pages/project-details/components/project-models/train-model-dialog/advanced-settings/ui/parameters.component.tsx
@@ -14,7 +14,7 @@ import { Tooltip } from './tooltip.component';
 
 interface ParametersProps {
     parameters: ConfigurationParameter[];
-    onChange: () => void;
+    onChange: (value: string | boolean | number) => void;
 }
 
 const ParameterTooltip: FC<{ text: string }> = ({ text }) => {
@@ -23,7 +23,8 @@ const ParameterTooltip: FC<{ text: string }> = ({ text }) => {
 
 interface ParameterProps {
     parameter: ConfigurationParameter;
-    onChange: () => void;
+    onChange: (value: string | boolean | number) => void;
+    isDisabled?: boolean;
 }
 
 interface ParameterLayoutProps {
@@ -46,13 +47,14 @@ const ParameterLayout: FC<ParameterLayoutProps> = ({ header, children, descripti
     );
 };
 
-const ParameterField: FC<ParameterProps> = ({ parameter, onChange }) => {
+const ParameterField: FC<ParameterProps> = ({ parameter, onChange, isDisabled }) => {
     if (parameter.type === 'enum') {
         return (
             <ToggleButtons
                 options={parameter.allowedValues}
                 selectedOption={parameter.value}
                 onOptionChange={onChange}
+                isDisabled={isDisabled}
             />
         );
     }
@@ -65,29 +67,59 @@ const ParameterField: FC<ParameterProps> = ({ parameter, onChange }) => {
                 maxValue={parameter.maxValue}
                 onChange={onChange}
                 type={parameter.type}
+                isDisabled={isDisabled}
             />
         );
     }
 
     if (parameter.type === 'bool') {
-        return <BooleanParameter value={parameter.value} header={parameter.name} onChange={() => {}} />;
+        return (
+            <BooleanParameter
+                value={parameter.value}
+                header={parameter.name}
+                onChange={onChange}
+                isDisabled={isDisabled}
+            />
+        );
     }
 };
 
-const Parameter: FC<ParameterProps> = ({ parameter, onChange }) => {
+export const Parameter = ({ parameter, onChange, isDisabled }: ParameterProps) => {
+    const handleReset = () => {
+        onChange(parameter.defaultValue);
+    };
+
     return (
-        <ParameterLayout header={parameter.name} description={parameter.description} onReset={() => {}}>
-            <ParameterField parameter={parameter} onChange={onChange} />
+        <ParameterLayout header={parameter.name} description={parameter.description} onReset={handleReset}>
+            <ParameterField parameter={parameter} onChange={onChange} isDisabled={isDisabled} />
         </ParameterLayout>
     );
 };
 
-export const Parameters: FC<ParametersProps> = ({ parameters, onChange }) => {
+Parameter.Layout = ParameterLayout;
+Parameter.Field = ParameterField;
+
+interface ParametersListProps {
+    parameters: ConfigurationParameter[];
+    onChange: (value: string | boolean | number) => void;
+}
+
+const ParametersList = ({ parameters, onChange }: ParametersListProps) => {
+    return parameters.map((parameter) => <Parameter key={parameter.name} parameter={parameter} onChange={onChange} />);
+};
+
+export const Parameters = ({ parameters, onChange }: ParametersProps) => {
     return (
         <Grid columns={['size-3000', minmax('size-3400', '1fr'), 'size-400']} gap={'size-300'} alignItems={'center'}>
-            {parameters.map((parameter) => (
-                <Parameter key={parameter.name} parameter={parameter} onChange={onChange} />
-            ))}
+            <ParametersList parameters={parameters} onChange={onChange} />
+        </Grid>
+    );
+};
+
+Parameters.Container = ({ children }: { children: ReactNode }) => {
+    return (
+        <Grid columns={['size-3000', minmax('size-3400', '1fr'), 'size-400']} gap={'size-300'} alignItems={'center'}>
+            {children}
         </Grid>
     );
 };
